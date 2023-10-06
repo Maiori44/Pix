028d9e6b4df42014a94e7892f860ec350b88d444
Maiori44 <felice.dangelo2013@gmail.com>
2023-10-06 20:22:26 +0200

    add dots animation

--- src/script.js
+++ src/script.js
@@ -7,7 +7,12 @@ form.addEventListener("submit", e => {
 		form.style.opacity = parseFloat(form.style.opacity) - 0.05
 		text.style.opacity = parseFloat(text.style.opacity) - 0.05
 		if (parseFloat(form.style.opacity) <= 0) {
-			form.innerHTML = "<h2>Loading...</h2>"
+			form.innerHTML = "<h2>Uploading</h2>"
+			let dots = 0
+			const dots_interval = window.setInterval(() => {
+				dots = (dots + 1) % 4
+				form.innerHTML = "<h2>Uploading" + ".".repeat(dots) + "</h2>"
+			}, 500)
 			const appear_interval = window.setInterval(() => {
 				form.style.opacity = parseFloat(form.style.opacity) + 0.05
 				if (parseFloat(form.style.opacity) >= 1) {
