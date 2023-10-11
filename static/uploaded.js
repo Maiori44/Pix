4f659da12d20ae5dab8bf47401b84b01b9e13838
Maiori44 <felice.dangelo2013@gmail.com>
2023-10-11 22:35:21 +0200

    finish copy link button

--- static/uploaded.js
+++ static/uploaded.js
@@ -1,7 +1,13 @@
 const file = new URLSearchParams(window.location.search).get("file")
-console.log(file)
 if (!file) {
 	document.getElementById("file-uploaded-text").innerText = "What are you doing here?"
 } else {
 	document.getElementById("uploaded-file").src = "/files/" + file
 }
+
+const copy_link_button = document.getElementsByClassName("uploaded-button")[0]
+copy_link_button.addEventListener("click", () => {
+	navigator.clipboard.writeText(window.location.protocol + "//" + window.location.host + "/files/" + file)
+	copy_link_button.innerText = "Link copied!"
+	copy_link_button.classList.add("disabled-button")
+})
