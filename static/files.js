37258f38f63acea52c0a50dbe46159b52597d92e
Maiori44 <felice.dangelo2013@gmail.com>
2023-10-14 18:19:06 +0200

    show upload date

--- static/files.js
+++ static/files.js
@@ -17,17 +17,24 @@ password_check.addEventListener("submit", e => {
 			document.getElementById("title").innerText = "Currently uploaded files"
 			document.getElementById("file-list").insertAdjacentHTML("beforeend", text)
 			const img = document.getElementById("preview-file")
+			const preview_text = document.getElementById("preview-text")
 			img.style.opacity = "0"
 			let target_opacity = 0
 			let next_target_opacity = 0
 			let old_src
 			Array.from(document.getElementsByClassName("link")).forEach(link => {
+				const upload_date = new Date(parseInt(link.href.match(/(\d+)-.+$/)[1]) * 1000)
 				link.addEventListener("mouseenter", () => {
+					preview_text.innerText = "Upload date: " + upload_date.toLocaleString()
+					preview_text.style.opacity = "1"
+					preview_text.style.animation = "fade-in 140ms linear"
 					old_src = img.src
 					img.src = link.href
 					next_target_opacity = 1
 				})
 				link.addEventListener("mouseleave", () => {
+					preview_text.style.opacity = "0"
+					preview_text.style.animation = "fade-out 140ms linear"
 					next_target_opacity = 0
 					target_opacity = 0
 				})
