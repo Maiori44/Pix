900ab86e0fbc64f7fa76dfd2c203721afaff382a
Maiori44 <felice.dangelo2013@gmail.com>
2023-10-14 18:33:37 +0200

    refactor uploading animation to use CSS

--- static/index.js
+++ static/index.js
@@ -2,27 +2,20 @@ const form = document.getElementById("upload-form")
 
 form.addEventListener("submit", _ => {
 	const text = document.getElementById("upload-disappear")
-	form.style.opacity = 1
-	text.style.opacity = 1
-	const disappear_interval = window.setInterval(() => {
-		form.style.opacity = parseFloat(form.style.opacity) - 0.05
-		text.style.opacity = parseFloat(text.style.opacity) - 0.05
-		if (parseFloat(form.style.opacity) <= 0) {
-			form.innerHTML = "<h2>Uploading</h2>"
-			let dots = 0
-			window.setInterval(() => {
-				dots = (dots + 1) % 4
-				form.innerHTML = "<h2>Uploading" + ".".repeat(dots) + "</h2>"
-			}, 500)
-			const appear_interval = window.setInterval(() => {
-				form.style.opacity = parseFloat(form.style.opacity) + 0.05
-				if (parseFloat(form.style.opacity) >= 1) {
-					window.clearInterval(appear_interval)
-				}
-			}, 7)
-			window.clearInterval(disappear_interval)
-		}
-	}, 7)
+	form.addEventListener("animationend", () => {
+		form.innerHTML = "<h2>Uploading</h2>"
+		let dots = 0
+		window.setInterval(() => {
+			dots = (dots + 1) % 4
+			form.innerHTML = "<h2>Uploading" + ".".repeat(dots) + "</h2>"
+		}, 500)
+		form.style.opacity = "1"
+		form.style.animation = "fade-in 140ms linear"
+	}, {once: true})
+	form.style.opacity = "0"
+	text.style.opacity = "0"
+	form.style.animation = "fade-out 140ms linear"
+	text.style.animation = "fade-out 140ms linear"
 })
 
 const fieldset = document.getElementById("fieldset")
