72f0dd6319b0e23ec6295358bed588c464fba90b
Maiori44 <felice.dangelo2013@gmail.com>
2023-10-20 11:31:03 +0200

    fix deleting files in new database

--- src/database.sql
+++ src/database.sql
@@ -1,14 +1,16 @@
 ---init:
 CREATE TABLE IF NOT EXISTS Files (
-    uuid BLOB NOT NULL,
-    filename TEXT NOT NULL,
-    content_type TEXT,
-    upload_date INTEGER,
-    owner TEXT,
-    PRIMARY KEY(uuid)
+	uuid BLOB NOT NULL,
+	filename TEXT NOT NULL,
+	content_type TEXT,
+	upload_date INTEGER,
+	owner TEXT,
+	PRIMARY KEY(uuid)
 );
 ---get_info:
 SELECT content_type, upload_date, owner FROM Files WHERE uuid = ?
 ---add_file:
 INSERT INTO Files VALUES (?, ?, ?, ?, ?);
+---delete_file:
+DELETE FROM Files WHERE uuid = ?
 ---
