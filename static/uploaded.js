dfa52ce311ef10d6d1b713291b77749d6158bd07
Maiori44 <felice.dangelo2013@gmail.com>
2023-10-27 18:16:42 +0200

    test clipboard fix

--- static/uploaded.js
+++ static/uploaded.js
@@ -18,7 +18,19 @@ if (!file) {
 
 const copy_link_button = document.getElementsByClassName("two-buttons")[0]
 copy_link_button.addEventListener("click", () => {
-	navigator.clipboard.writeText(window.location.protocol + "//" + window.location.host + file)
-	copy_link_button.innerText = "Link copied!"
-	copy_link_button.classList.add("disabled-button")
-})
+	if (!navigator.clipboard) {
+		document.getElementsByClassName("invisible")[0].remove()
+		title.parentElement.innerHTML = "<span class=\"bigger\">Couldn't copy to clipboard.</span><br>"
+		+ "Your browser may be restricting Pix's access to the clipboard because you're using an http "
+		+ "connection instead of an https one.<br>"
+		copy_link_button.innerText = "Try using https"
+		copy_link_button.style.animation = "fade-white-yellow 0.5s forwards"
+		copy_link_button.addEventListener("click", () => {
+			window.location.href = window.location.href.replace("http://", "https://")
+		})
+	} else {
+		navigator.clipboard.writeText(window.location.protocol + "//" + window.location.host + file)
+		copy_link_button.innerText = "Link copied!"
+		copy_link_button.classList.add("disabled-button")
+	}
+}, { once: true })
