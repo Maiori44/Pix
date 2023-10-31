fd79d133afa823dfb129fce0ffa29c0d1b63d240
Maiori44 <felice.dangelo2013@gmail.com>
2023-10-31 18:20:14 +0100

    refactor file sending logic

--- static/upload_logic.js
+++ static/upload_logic.js
@@ -35,6 +35,7 @@ function set_upload_file_logic(form, replace) {
 			}
 		}, 1000)
 		let waiting_logs = []
+
 		function log(message) {
 			if (list) {
 				if (waiting_logs) {
@@ -51,97 +52,92 @@ function set_upload_file_logic(form, replace) {
 				waiting_logs.push(message)
 			}
 		}
-		async function send_fragment(reader, name, i, id) {
-			if (errored)
-				return;
-			const { value: value1, done: done1 } = await reader.read(new Uint8Array(65536))
-			const { value: value2, done: done2 } = await reader.read(new Uint8Array(65536))
-			const buffer = new Uint8Array(value1.length + value2.length)
-			buffer.set(value1, 0)
-			buffer.set(value2, value1.length)
-			const not_done = !(done1 && done2)
-			if (not_done && !errored) {
-				log(`Sending ${name}'s fragment n°${i}...`)
-				fetch(`/upload/${id}/fragment`, {
-					method: "POST",
-					body: buffer,
-					headers: {
-						Password: password,
-						FragmentNum: i++,
-					}
-				}).then(async result => {
-					if (result.status != 204) {
-						if (errored)
-							return
-						errored = true
-						const result_body = await result.text()
-						document.write(
-							replace
-								? result_body.replace("/index.html", "/files.html")
-								: result_body
-						)
-						return
-					}
-					log(`${name}'s fragment n°${i - 1} was received!`)
-					uploaded_size += buffer.length
-					if (percentage) {
-						const amount = Math.floor((uploaded_size / total_size) * 100000) / 1000
-						percentage.innerText = total_files > 1
-							? `${amount}% (${uploaded_files}/${total_files})`
-							: `${amount}%`
-					}
-				})
-				return send_fragment(reader, name, i, id)
+
+		function update_percentage() {
+			if (percentage) {
+				const amount = Math.floor((uploaded_size / total_size) * 100000) / 1000
+				percentage.innerText = total_files > 1
+					? `${amount}% (${uploaded_files}/${total_files})`
+					: `${amount}%`
 			}
 		}
+
 		let names = []
-		for (const file of files.files) {
+
+		async function send_file(file) {
 			total_files += 1
 			total_size += file.size
+			const reader = file.stream().getReader({ mode: "byob" })
 			const id = crypto.getRandomValues(new BigUint64Array(2)).join("")
-			send_fragment(
-				file.stream().getReader({ mode: "byob" }),
-				file.name,
-				1,
-				id
-			).then(async () => {
-				if (errored)
-					return;
-				const finish_result = await fetch(`/upload/${id}/${replace ? "replace" : "finish"}`, {
-					method: "POST",
-					body: `${file.type}\n${replace ?? file.name}`,
-					headers: {
-						Password: password,
-						IP: ip
-					}
-				})
-				log(`All of ${file.name}'s fragments were sent! Finishing upload...`)
-				const result_body = await finish_result.text()
-				if (finish_result.status != 200) {
-					if (errored)
-						return
-					errored = true
-					document.write(replace ? result_body.replace("/index.html", "/files.html") : result_body)
-					return
-				}
-				log(`${file.name} finished uploading!`)
-				uploaded_files += 1
-				if (percentage) {
-					const amount = Math.floor((uploaded_size / total_size) * 100000) / 1000
-					percentage.innerText = total_files > 1
-						? `${amount}% (${uploaded_files}/${total_files})`
-						: `${amount}%`
-				}
-				names.push(result_body)
-				if (uploaded_files == total_files) {
-					let args = ""
-					for (const [i, name] of names.entries()) {
-						args += `file${i}=${encodeURIComponent(name)}&`
-					}
-					if (!errored)
-						window.location.href = `/uploaded.html?${args}replaced=${!!replace}&total=${names.length}`
+			const fragments = []
+			let i = 1
+			while (!errored) {
+				const { value: value1, done: done1 } = await reader.read(new Uint8Array(65536))
+				const { value: value2, done: done2 } = await reader.read(new Uint8Array(65536))
+				const buffer = new Uint8Array(value1.length + value2.length)
+				buffer.set(value1, 0)
+				buffer.set(value2, value1.length)
+				if (!(done1 && done2)) {
+					log(`Sending ${file.name}'s fragment n°${i}...`)
+					fragments.push(fetch(`/upload/${id}/fragment`, {
+						method: "POST",
+						body: buffer,
+						headers: {
+							Password: password,
+							FragmentNum: i++,
+						}
+					}).then(async result => {
+						if (result.status != 204) {
+							if (errored) return
+							errored = true
+							const result_body = await result.text()
+							document.write(
+								replace
+									? result_body.replace("/index.html", "/files.html")
+									: result_body
+							)
+							return
+						}
+						log(`${file.name}'s fragment n°${i - 1} was received!`)
+						uploaded_size += buffer.length
+						update_percentage()
+					}))
+				} else break
+			}
+			log(`All of ${file.name}'s fragments were sent! Waiting for receival...`)
+			for (const promise of fragments) {
+				if (errored) return
+				await promise
+			}
+			log(`All of ${file.name}'s fragments were received! Finishing upload...`)
+			const finish_result = await fetch(`/upload/${id}/${replace ? "replace" : "finish"}`, {
+				method: "POST",
+				body: `${file.type}\n${replace ?? file.name}`,
+				headers: {
+					Password: password,
+					IP: ip
 				}
 			})
+			const result_body = await finish_result.text()
+			if (finish_result.status != 200) {
+				if (errored) return
+				errored = true
+				document.write(replace ? result_body.replace("/index.html", "/files.html") : result_body)
+				return
+			}
+			log(`${file.name} finished uploading!`)
+			uploaded_files += 1
+			update_percentage()
+			names.push(result_body)
+			if (uploaded_files == total_files) {
+				let args = ""
+				for (const [i, name] of names.entries()) {
+					args += `file${i}=${encodeURIComponent(name)}&`
+				}
+				if (!errored)
+					window.location.href = `/uploaded.html?${args}replaced=${!!replace}&total=${names.length}`
+			}
 		}
+		for (const file of files.files) send_file(file)
 	})
 }
